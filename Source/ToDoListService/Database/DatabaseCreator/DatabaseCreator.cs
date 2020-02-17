64aab9eac83c6cd92603b01277419e8cbe2df422
marut97 <marut1997@gmail.com>
2020-02-17 21:55:18 +0530

    Database Creator Initial Commit. Contains Syntax of operations

--- Source/ToDoListService/Database/DatabaseCreator/DatabaseCreator.cs
+++ Source/ToDoListService/Database/DatabaseCreator/DatabaseCreator.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.SQLite;
 
 namespace ToDoListService.DatabaseCreator
 {
@@ -10,7 +6,21 @@ namespace ToDoListService.DatabaseCreator
     {
         static void Main(string[] args)
         {
+            SQLiteConnection.CreateFile("ToDoListDatabase.sqlite");
+            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=ToDoListDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
 
+            string sql = "create table highscores (name varchar(20), score int)";
+
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into highscores (name, score) values ('Me', 9001)";
+
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            m_dbConnection.Close();
         }
     }
 }
