6cbe925ee99c8e23b9591b1bd4e7d9d09b44f42b
marut97 <marut1997@gmail.com>
2020-02-17 23:18:47 +0530

    Database Creator updated until Passwords

--- Source/ToDoListService/Database/DatabaseCreator/DatabaseCreator.cs
+++ Source/ToDoListService/Database/DatabaseCreator/DatabaseCreator.cs
@@ -10,12 +10,19 @@ namespace ToDoListService.DatabaseCreator
             SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=ToDoListDatabase.sqlite;Version=3;");
             m_dbConnection.Open();
 
-            string sql = "create table highscores (name varchar(20), score int)";
+            string sql = "CREATE TABLE ToDoListData ("       +
+                            "Mode INT NOT NULL, "            +
+                            "Title VARCHAR(50) NOT NULL, "   +
+                            "Notes VARCHAR(MAX), "           +
+                            "Reminder DATETIME, "            +
+                            "Username VARCHAR(20) NOT NULL FOREIGN_KEY REFERENCES ToDoListAuthentication(Username))";
 
             SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
             command.ExecuteNonQuery();
 
-            sql = "insert into highscores (name, score) values ('Me', 9001)";
+            sql = "CREATE TABLE ToDoListAuthentication (" +
+                    "Username VARCHAR(20) NOT NULL" + 
+                    "Password VARCHAR(20) NOT NULL)";
 
             command = new SQLiteCommand(sql, m_dbConnection);
             command.ExecuteNonQuery();
