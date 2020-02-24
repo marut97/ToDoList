0a6b27f0b8d4898f5e94bc78ee221aebd6c12350
marut97 <marut1997@gmail.com>
2020-02-24 15:16:54 +0530

    ToDoList data structure project added

---
+++ Source/ToDoListService/Data/ToDoListData/ListMode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListData
+{
+    [DataContract]
+    [Flags]
+    public enum ListMode
+    {
+        [EnumMember]
+        PeriodicTasks,
+
+        [EnumMember]
+        DailyTasks,
+
+        [EnumMember]
+        WeeklyGoals,
+
+        [EnumMember]
+        LongTermGoals
+    }
+}
---
+++ Source/ToDoListService/Data/ToDoListData/ToDoListData.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+using ToDoListData;
+
+namespace ToDoListService.Data.ToDoListData
+{
+    [DataContract]
+    public struct ToDoListData
+    {
+        [DataMember]
+        public ListMode ListMode { get; set; }
+
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string Notes { get; set; }
+
+        [DataMember]
+        public string Reminder { get; set; }
+
+        [DataMember]
+        public string Username { get; set; }
+    }
+}
