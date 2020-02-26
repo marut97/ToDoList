3689ebdf9aef23863065efc8c09ac953c63efe47
Marut Prasad <marut.prasad@philips.com>
2020-02-26 09:35:24 +0530

    To Do List Repository Updated

--- Source/ToDoListService/Lib/ToDoListRepository/ToDoListRepository.cs
+++ Source/ToDoListService/Lib/ToDoListRepository/ToDoListRepository.cs
@@ -18,17 +18,74 @@ namespace ToDoListService.Lib.ToDoListRepository
 
         public bool StartTaskEditing(int userID, int taskID)
         {
-            throw new NotImplementedException();
+            try
+            {
+                int rowsAffected = 0;
+                using (SQLiteConnection dbConnection = new SQLiteConnection(m_connectionString))
+                {
+                    dbConnection.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand(dbConnection))
+                    {
+                        cmd.CommandText = "UPDATE ToDoListDataTable SET UnderModification = 1 " +
+                                          "WHERE UserID = '" + userID + "' AND TaskID = '" + taskID + "';";
+                        rowsAffected = cmd.ExecuteNonQuery();
+                    }
+                }
+                return (rowsAffected == 1);
+            }
+            catch (SQLiteException e)
+            {
+                //throw fault exception
+                return false;
+            }
         }
 
         public bool EndTaskEditing(int userID, int taskID)
         {
-            throw new NotImplementedException();
+            try
+            {
+                int rowsAffected = 0;
+                using (SQLiteConnection dbConnection = new SQLiteConnection(m_connectionString))
+                {
+                    dbConnection.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand(dbConnection))
+                    {
+                        cmd.CommandText = "UPDATE ToDoListDataTable SET UnderModification = 0 AND DateModified = datetime('now') " +
+                                          "WHERE UserID = '" + userID + "' AND TaskID = '" + taskID + "';";
+                        rowsAffected = cmd.ExecuteNonQuery();
+                    }
+                }
+                return (rowsAffected == 1);
+            }
+            catch (SQLiteException e)
+            {
+                //throw fault exception
+                return false;
+            }
         }
 
         public bool CreateToDoListTask(int userID, ToDoListDataModel data)
         {
-            throw new NotImplementedException();
+            try
+            {
+                int rowsAffected = 0;
+                using (SQLiteConnection dbConnection = new SQLiteConnection(m_connectionString))
+                {
+                    dbConnection.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand(dbConnection))
+                    {
+                        cmd.CommandText = "INSERT INTO ToDoListDataTable(Mode, Title, Notes, UserID, DateModified, StartTime, EndTime) " +
+                                          "VALUES("+data.ListMode+",'"+data.Title+"','"+data.Notes+"',"+userID+",'"+data.StartDate+"','"+data.EndDate+"');";
+                        rowsAffected = cmd.ExecuteNonQuery();
+                    }
+                }
+                return (rowsAffected == 1);
+            }
+            catch (SQLiteException e)
+            {
+                //throw fault exception
+                return false;
+            }
         }
 
         public bool DeleteToDoListTask(int userID, ToDoListDataModel data)
